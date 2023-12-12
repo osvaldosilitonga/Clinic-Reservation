69c32bebae23deaf6270b03e497015fb5a3545ab
osvaldosilitonga <osvaldosilitonga@gmail.com>
2023-12-12 14:44:44 +0700

    setup echo server

--- clinic/main.go
+++ clinic/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"clinic/helpers"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-playground/validator"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Validator = &helpers.CustomValidator{Validator: validator.New()}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	PORT := os.Getenv("SERVER_PORT")
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", PORT)))
 }
